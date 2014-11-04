8488108bba3d1fe6ec7b6d3a8bb75ae12338cce7
wgx <wgx998877@qq.com>
2014-11-04 15:34:58 +0800

    wgx first add

---
+++ util/readhdf.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# Filename: readhdf.py
+# Author: wgx
+from pyhdf.SD import SD,SDC
+import numpy as np
+def read(filename,fieldname):
+	hdf = SD(filename,SDC.READ)
+	data = hdf.select(fieldname)
+	return data[:]
