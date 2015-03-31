52f92511d3c936ab7d7b9a0970a833f2c22507d5
wgx-mac <wgx998877@qq.com>
2015-03-31 09:42:27 +0800

    spa

--- spa.py
+++ spa.py
@@ -3,7 +3,7 @@
 
 import numpy as np
 import datetime
-import util as u
+#import util as u
 from math import *
 
 PI = pi
@@ -88,6 +88,232 @@ def validate_input(spa):
         return 17
     return 0
 
+
+def rad2deg(radian):
+    return degrees(radian)
+
+
+def deg2rad(degree):
+    return radians(degree)
+
+
+def limit_degrees(degree):
+    degree /= 360.0
+    limited = 360.0 * (degree - floor(degree))
+    if limited <= 0:
+        limited += 360.0
+    return limited
+
+
+def limit_degrees180pm(degree):
+    degree /= 360.0
+    limited = 180.0 * (degree - floor(degree))
+    if limited < 0:
+        limited += 180.0
+    return limited
+
+
+def limit_zero2one(value):
+    limited = value - floor(value)
+    if limited < 0:
+        limited += 1.0
+    return limited
+
+
+def limit_minutes(minutes):
+    if minutes < -20.0:
+        minutes += 1440.0
+    elif minutes > 20.0:
+        minutes -= 1440.0
+    return minutes
+
+
+def julian_century(jd):
+    return (jd - 2451545.0) / 36525.0
+
+
+def dayfrac_to_local_hr(dayfrac, timezone):
+    return 24.0 * limit_zero2one(dayfrac + timezone / 24.0)
+
+
+def third_order_polynomial(a, b, c, d, x):
+    return ((a * x + b) * x + c) * x + d
+
+
+def julian_ephemeris_day(jd, delta_t):
+    return jd + delta_t / 86400.0
+
+
+def julian_ephemeris_century(jde):
+    return (jde - 2451545.0) / 36525.0
+
+
+def julian_ephemeris_millennium(jce):
+    return (jce / 10.0)
+
+
+def earth_values(term_sum, jme):
+    s = 0.0
+    for i in range(len(term_sum)):
+        s += term_sum[i] * pow(jme, i)
+    s /= 1.0e8
+    return s
+
+
+def earth_heliocentric_longitude(jme):
+    s = [0] * L_COUNT
+    for i in range(L_COUNT):
+        s[i] = earth_periodic_term_summation(L_TERMS[i], l_subcount[i], jme)
+    return limit_degrees(rad2deg(earth_values(s, L_COUNT, jme)))
+
+
+def earth_heliocentric_latitude(jme):
+    s = [0] * B_COUNT
+    for i in range(B_COUNT):
+        s[i] = earth_periodic_term_summation(L_TERMS[i], l_subcount[i], jme)
+    return rad2deg(earth_values(s, B_COUNT, jme))
+
+
+def earth_radius_vector(jme):
+    s = [0] * R_COUNT
+    for i in range(R_COUNT):
+        s[i] = earth_periodic_term_summation(R_TERMS[i], r_subcount[i], jme)
+    return earth_values(s, R_COUNT, jme)
+
+
+def geocentric_longitude(l):
+    theta = l + 180.0
+    if theta >= 360.0:
+        theta -= 360.0
+    return theta
+
+
+def geocentric_latitude(b):
+    return -b
+
+
+def mean_elongation_moon_sun(jce):
+    return third_order_polynomial(
+        1.0 / 189474.0, -0.0019142, 445267.11148, 297.85036, jce)
+
+
+def mean_anomaly_sun(jce):
+    return third_order_polynomial(-1.0 / 300000.0, -
+                                  0.0001603, 35999.05034, 357.52772, jce)
+
+
+def mean_anomaly_moon(jce):
+    return third_order_polynomial(
+        1.0 / 56250.0, 0.0086972, 477198.867398, 134.96298, jce)
+
+
+def argument_latitude_moon(jce):
+    return third_order_polynomial(
+        1.0 / 327270.0, -0.0036825, 483202.017538, 93.27191, jce)
+
+
+def ascending_longitude_moon(jce):
+    return third_order_polynomial(
+        1.0 / 450000.0, 0.0020708, -1934.136261, 125.04452, jce)
+
+
+def xy_term_summation(i, x):
+    s = 0
+    for j in range(TERM_Y_COUNT):
+        s += x[j] * Y_TERMS[i][j]
+    return s
+
+
+def nutation_longitude_and_obliquity(jce, x):
+    sum_psi = 0
+    sum_epsilon = 0
+    for i in range(Y_COUNT):
+        xy_term_sum = deg2rad(xy_term_summation(i, x))
+        sum_psi += (PE_TERMS[i][TERM_PSI_A] + jce *
+                    PE_TERMS[i][TERM_PSI_B]) * sin(xy_term_sum)
+        sum_epsilon += (PE_TERMS[i][TERM_EPS_C] + jce *
+                        PE_TERMS[i][TERM_EPS_D]) * cos(xy_term_sum)
+    return sum_psi / 36000000.0, sum_epsilon / 36000000.0
+
+
+def ecliptic_mean_obliquity(jme):
+    u = jme / 10.0
+    return 84381.448 + u * (-4680.93 + u * (-1.55 + u * (1999.25 + u * (-51.38 + u *
+                                                                        (-249.67 + u * (-39.05 + u * (7.12 + u * (27.87 + u * (5.79 + u * 2.45)))))))))
+
+
+def ecliptic_true_obliquity(del_epsilon, epsilon0):
+    return delta_epsilon + epsilon0 / 3600.0
+
+
+def aberration_correction(r):
+    return -20.4898 / (3600.0 * r)
+
+
+def apparent_sun_longitude(theta, del_psi, del_tau):
+    return theta + delta_psi + delta_tau
+
+
+def greenwich_mean_sidereal_time(jd, jc):
+    return limit_degrees(280.46061837 + 360.98564736629 *
+                         (jd - 2451545.0) + jc * jc(0.000387933 - jc / 38710000.0))
+
+
+def greenwich_sidereal_time(nu0, del_psi, epsilon):
+    return nu0 + delta_psi * cso(deg2rad(epsilon))
+
+
+def geocentric_sun_right_ascension(lamda, epsilon, beta):
+    lamda_rad = deg2rad(lamda)
+    epsilon_rad = deg2rad(epsilon)
+    return limit_degrees(rad2deg(
+        atan2(sin(lamda_rad) * cos(epsilon_rad) - tan(deg2rad(beta) * sin(epsilon_rad)))))
+
+
+def geocentric_sun_declination(beta, epsilon, lamda):
+    beta_rad = deg2rad(beta)
+    epsilon_rad = deg2rad(epsilon)
+    return rad2deg(asin(sin(beta_rad) * cos(epsilon_rad) +
+                        cos(beta_rad) * sin(epsilon_rad) * sin(deg2rad(lamda))))
+
+
+def observer_hour_angle(nu, longitude, alpha_deg):
+    return limit_degrees(nu + longitude - alpha_deg)
+
+
+def sun_right_ascension_parallax_and_topocentric_dec(
+        latitude, elevation, xi, h, delta):
+    lat_rad = deg2rad(latitude)
+    xi_rad = deg2rad(xi)
+    h_rad = deg2rad(h)
+    delta_rad = deg2rad(delta)
+    u = atan(0.99664719 * tan(lat_rad))
+    y = 0.99664719 * sin(u) + elevation * sin(lat_rad) / 6378140.0
+    x = cos(u) + elevation * cos(lat_rad) / 6378140.0
+    delta_alpha_rad = atan2(-
+                            x *
+                            sin(xi_rad) *
+                            sin(h_rad), cos(delta_rad) -
+                            x *
+                            sin(xi_rad) *
+                            cos(h_rad))
+    return rad2deg(atan2((sin(delta_rad) - y*sin(xi_rad) * cos(delta_alpha_rad),
+                         cos(delta_rad) - x*sin(xi_rad) * cos(h_rad)
+                         ))), rad2deg(delta_alpha_rad)
+
+def topocentric_sun_right_ascension(alpha_deg, delta_alpha):
+    return alpha_deg + delta_alpha
+
+def topocentric_elevation_angle(latitude, delta_prime, h_prime):
+    lat_rad = deg2rad(latitude)
+    delta_prime_rad = deg2rad(delta_prime)
+    return rad2deg(asin(sin(lat_rad)* sin(delta_prime_rad)+cos(lat_rad)*cos(delta_prime_rad)*cos(deg2rad(h_prime))))
+
+def topocentric_zenith_angle(e):
+    return 90.0 - e
+
+def topocentric_elevation_angle_corrected(e0, delta_e):
+    return e0 + delta_e
 
 def calculate_geocentric_sun_right_ascension_and_declination(spa):
     x = []
