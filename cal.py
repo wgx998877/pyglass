aa0b7aad9017872f3aebd645461e606311c0beb0
wgx998877 <wgx998877@qq.com>
2015-05-12 11:46:04 +0800

    'wgx'

--- cal.py
+++ cal.py
@@ -3,7 +3,7 @@
 # Filename: cal.py
 # Author: wgx
 import numpy as np
-
+import math
 alp = 10000
 
 def leastsq(x,y):
@@ -16,7 +16,7 @@ def leastsq(x,y):
     xsum = 0.0
     ysum = 0.0
 
-    for i in range(len(x)):
+    for i in xrange(len(x)):
         xsum += (x[i] - meanx)*(y[i]-meany)
         ysum += (x[i] - meanx)**2
 
@@ -64,7 +64,47 @@ def r2(x,y):
         x2 += (x[i]-mx)**2
         y2 += (y[i]-my)**2
     return (t2/((x2*y2)**0.5))**2
-
+    
+def mk(x):
+    n = len(x)
+    S = 0
+    for i in xrange(0, n):
+        for j in xrange(i+1, n):
+            sgn = 0
+            if x[j] > x[i]: sgn = 1
+            elif x[j] == x[i]: sgn = 0
+            else : sgn = -1
+            S += sgn
+    Zc = 0
+    if S > 0:
+        Zc = (S - 1)/((n*(n-1)*(2*n+5))**0.5)
+    elif S < 0:
+        Zc = (S + 1)/((n*(n-1)*(2*n+5))**0.5)
+    else :
+        Zc = 0
+    return Zc
+   
+def RS(x):
+    n = len(x)
+    if n <= 0 : return
+    RS = []
+    XS = []
+    for t in xrange(2,n+1):
+        es = sum(x[:t])*1.0/t
+        st = 0.0
+        for j in xrange(t):
+            st += (x[j] - es)**2
+        st = (st*1.0/t)**0.5
+        X = []
+        s = 0
+        for j in xrange(t):
+            s+=x[j]
+            X.append((s-(j+1)*es))
+        rt = max(X[:t])*1.0 - min(X[:t])*1.0
+        RS.append(math.log(rt*1.0/st))      
+        XS.append(math.log(t))
+    return leastsq(XS, RS)
+   
 def pca(data,topNfeat=999999):
     meanV = np.mean(data,axis=0)
     meanR = data - meanV
